ae8abff8ce5bc61d71c8d9d7a53205fd399db644
Fred Morris <consulting@m3047.net>
2022-07-19 10:59:32 -0700

    GNU Affero GPL.

--- python/rkvdns/statistics.py
+++ python/rkvdns/statistics.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-# Copyright (c) 2020 by Fred Morris Tacoma WA
+# Copyright (c) 2020-2022 by Fred Morris Tacoma WA
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version 3,
+# as published by the Free Software Foundation.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """Collecting and Reporting Operational Statistics.
 
